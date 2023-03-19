b91805c827755674f77987ac1529fc84222f6e88
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2023-03-19 18:53:24 +0100

    Barra navegacion desplegable

---
+++ app.js
@@ -0,0 +1,27 @@
+const miapp = Vue.createApp({
+        data () {
+            return{
+                showNav: false
+            };
+        },
+        // watch: { //cuando cambia la data el watcher numero se ejecuta
+        //   numero(nuevoValor, valorAnterior){
+        //     console.log("El numero ha pasado de ser %s a %s", valorAnterior, nuevoValor);
+        //   }
+        // },
+        methods: {
+            ocultar(){
+                this.showNav = !this.showNav
+            }
+        },
+        computed:{
+            colorTexto: function (){
+                return (this.contador < 0) ? 'red':'blue'
+            }
+        }
+    },
+);
+
+miapp.mount('#app');
+
+
