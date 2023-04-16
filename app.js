e9e478af43be59f691cf995dd16ad18788d66655
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2023-04-16 08:45:27 +0200

    Boton eliminar, Dialogo confirmacion y arreglo imagenes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const miapp = Vue.createApp({
         data () {
             return{
-                showNav: false,
+                showNav: true,
+                showGat: false,
                 nom: '',
                 edat: '',
                 gats:{
-                }
+                    'Mishi':2
+                },
+                gat: '',
+                idx: 0
             };
         },
         // watch: { //cuando cambia la data el watcher numero se ejecuta
@@ -17,17 +21,43 @@ const miapp = Vue.createApp({
             ocultar(){
                 this.showNav = !this.showNav
             },
+            getIndexGat: function(index){
+                return index + 1;
+            },
             agregarGat: function(){
                 this.gats[this.nom] = this.edat;
+                this.showGat = true;
                 this.nom = "";
                 this.edat = "";
-            }
+            },
+            setGat(nom){
+                this.gat = nom;
+            },
+            eliminarGat(){
+                delete this.gats[this.gat];
+                this.gat = '';
+            },
+            rellenarGat(nom,edat){
+                this.nom = nom;
+                this.edat = edat;
+            },
+
+
+
         },
         computed:{
         }
     },
 );
+function mostrarDialogo() {
+    var miDialogo = document.getElementById("miDialogo");
+    miDialogo.showModal();
+}
 
+function cerrarDialogo() {
+    var miDialogo = document.getElementById("miDialogo");
+    miDialogo.close();
+}
 miapp.mount('#app');
 
 
